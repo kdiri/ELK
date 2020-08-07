805dc4b2557609f370de6ae599cc4eb4472744ed
Kemal Diri <kemaldiri@gmail.com>
2020-08-07 09:15:40 +0200

    Add grok and change logstash port.

--- src/helper/logging_helper.py
+++ src/helper/logging_helper.py
@@ -7,5 +7,5 @@ import os
 from loguru import logger
 
 LOG_PATH = os.path.join("logs", "flask_api.log")
-logger.add(LOG_PATH, format="[{time}] - [{level.no}] - [{level}] - [{message}]")
+logger.add(LOG_PATH, format="[{time:YYYY-MM-DD HH:mm:ss}] - [{level.no}] - [{level}] - [{message}]")
 new_logger = logger
